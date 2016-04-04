b850b39ab263f4ba0abcde0a75775ee58b0c1a6a
UncP <UncP@users.noreply.github.com>
2016-04-04 12:52:52 +0800

    过滤图片

--- zhihu_collection.py
+++ zhihu_collection.py
@@ -4,20 +4,18 @@ import re
 def get_collection(url):
 	html = urlopen(url)
 	text = html.read().decode('utf-8', 'ignore')
-	text = text.replace('&lt;br&gt;', '\n')
-	text = text.replace('&lt;/b&gt;', '\n')
-	text = text.replace('&lt;b&gt;', '\n')
-	pattern = re.compile(r'((?<=<h2 class="zm-item-title"><a target="_blank" href="/question/\d{7}">)(.*?)(?=</a></h2>))|((?<=<h2 class="zm-item-title"><a target="_blank" href="/question/\d{8}">)(.*?)(?=</a></h2>))|((?<=<textarea class="content hidden">)(.*?)(?=<span class="answer-date-link-wrap">))', re.S)
-	obj = re.findall(pattern, text)
+	qu_an = re.compile(r'((?<="/question/\d{6}">)(.*?)(?=</a></h2>))|((?<="/question/\d{7}">)(.*?)(?=</a></h2>))|((?<="/question/\d{8}">)(.*?)(?=</a></h2>))|((?<=<textarea class="content hidden">)(.*?)(?=<span class="answer-date-link-wrap">))', re.S)
+	obj = re.findall(qu_an, text)
 	next_page = re.findall(r'\?page=\d{1,}(?=">下一页)', text)
-	f = open('temp.txt', 'w', encoding = 'utf-8')
+	f = open('zhihu.txt', 'w', encoding = 'utf-8')
 	for each in obj:
-	    f.write(each[0] + each[2] + each[4])
-	    f.write('\n---------------\n')
+		answer = re.sub(r'&lt;(.+?)&gt;','\n',each[6])
+		f.write(each[4] + answer)
+		f.write('\n---------------\n')
 	f.close()
 	return next_page
 
-number = '28944253'
+number = '67808231'
 url = 'https://www.zhihu.com/collection/'
 url = url + number
 page = ''
@@ -28,4 +26,3 @@ while True:
 	else:
 		page = next_page[0]
 		input('press Enter key to continue ...\n')
-
